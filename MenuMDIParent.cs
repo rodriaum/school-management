0ec6b9b1327b1d09fc0e7b18f7b703f23dddbfa8
Rodrigo Ferreira <rodriaum.school@gmail.com>
2025-09-22 14:34:18 +0100

    Adicionar arquivos de projeto.

---
+++ MenuMDIParent.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace GestaoEscolaAula
+{
+    public partial class MenuMDIParent : Form
+    {
+        private int childFormNumber = 0;
+
+        public MenuMDIParent()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowNewForm(object sender, EventArgs e)
+        {
+            Form childForm = new Form();
+            childForm.MdiParent = this;
+            childForm.Text = "Janela " + childFormNumber++;
+            childForm.Show();
+        }
+
+        private void OpenFile(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            openFileDialog.Filter = "Arquivos de texto (*.txt)|*.txt|Todos os arquivos (*.*)|*.*";
+
+            if (openFileDialog.ShowDialog(this) == DialogResult.OK)
+            {
+                string FileName = openFileDialog.FileName;
+            }
+        }
+    }
+}
