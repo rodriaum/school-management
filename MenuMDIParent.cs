3974527dc36ef0063dd331f52745b4aecec50f96
Rodrigo Ferreira <rodriaum.school@gmail.com>
2025-09-26 17:11:08 +0100

    Remove unused functions

--- MenuMDIParent.cs
+++ MenuMDIParent.cs
@@ -30,27 +30,6 @@ namespace GestaoEscolaAula
             }
         }
 
-        private void ShowNewForm(object sender, EventArgs e)
-        {
-            Form childForm = new Form();
-            childForm.MdiParent = this;
-            childForm.Text = "Janela " + childFormNumber++;
-            childForm.Show();
-        }
-
-        private void OpenFile(object sender, EventArgs e)
-        {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-
-            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
-            openFileDialog.Filter = "Arquivos de texto (*.txt)|*.txt|Todos os arquivos (*.*)|*.*";
-
-            if (openFileDialog.ShowDialog(this) == DialogResult.OK)
-            {
-                string FileName = openFileDialog.FileName;
-            }
-        }
-
         private void listarCursoToolStripMenuItem_Click(object sender, EventArgs e)
         {
             ListCourseForm form = new ListCourseForm(_sqlService);
